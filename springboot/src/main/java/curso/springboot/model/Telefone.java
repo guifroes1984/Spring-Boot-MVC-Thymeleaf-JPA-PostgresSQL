255a0ea0ea2d3e188817435a4c36d544ee436657
guifroes1984 <guilhermefroes26@gmail.com>
2022-12-10 01:36:40 -0300

    Removendo telefones do usuário em cascata

---
+++ springboot/src/main/java/curso/springboot/model/Telefone.java
@@ -0,0 +1,59 @@
+package curso.springboot.model;
+
+import org.hibernate.annotations.ForeignKey;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@SuppressWarnings("removal")
+@Entity
+public class Telefone {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String numero;
+
+	private String tipo;
+
+	@ManyToOne
+	@ForeignKey(name = "pessoa_id")
+	private Pessoa pessoa;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+}
