88faae4052237beb3d1781fa10a37f950afcdad8
guifroes1984 <guilhermefroes26@gmail.com>
2022-12-14 17:06:10 -0300

    Criando combo de sexo no cadastro.

--- springboot/src/main/java/curso/springboot/model/Pessoa.java
+++ springboot/src/main/java/curso/springboot/model/Pessoa.java
@@ -48,6 +48,16 @@ public class Pessoa implements Serializable {
 
 	private String ibge;
 
+	private String sexopessoa;
+
+	public String getSexopessoa() {
+		return sexopessoa;
+	}
+
+	public void setSexopessoa(String sexopessoa) {
+		this.sexopessoa = sexopessoa;
+	}
+
 	public String getCep() {
 		return cep;
 	}
